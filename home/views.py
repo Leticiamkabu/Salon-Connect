ba1620cd9b9087cbd7bd29c9f640beddfd50a937
Leticiamkabu <48065544+Leticiamkabu@users.noreply.github.com>
2022-08-03 09:15:57 +0100

    added the user dashboard

--- home/views.py
+++ home/views.py
@@ -6,7 +6,7 @@ def landing_page(request):
     return render(request, "home/index.html")
 
 def user_page_view(request):
-    return render(request, "user/user_page.html")
+    return render(request, "user/pages/user_dashboard.html")
 
 def service_provider_view(request):
     return render(request, "service_provider/service_provider_page.html")
